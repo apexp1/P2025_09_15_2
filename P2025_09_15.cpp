e52f06d1a65aea66832ddb13fbdd8c8a249724a8
박 박한승 <apexp1@naver.com>
2025-09-22 16:39:36 +0900

    2025년 9월 22일

--- P2025_09_15.cpp
+++ P2025_09_15.cpp
@@ -1,13 +1,102 @@
 // P2025_09_15.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <iostream>  //CPP 표준
+#include <conio.h>   //C 표준
 
-int main()
-{
-    //std::cout << "Hello World!\n";
-    std::cout << "안녕하세요..!\n";
+
+using namespace std;
+
+
+
+int World[10][10] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+};
+
+
+
+bool bIsRunning  = true;
+int PlayerX      = 1;
+int PlayerY      = 1;
+char PlayerShape = 'P';
+
+//사상(Mapping
+char Sprites[2] = { ' ', '*' }; //그림으로 바꾸자
+
+
+void Input() {
+
+    char KeyCode = _getch();
+
+    switch (KeyCode)
+    {
+    case 'w':
+        PlayerX--;
+        break;
+
+    case 'a':
+        PlayerY--;
+        break;
+
+    case 's':
+        PlayerX++;
+        break;
+
+    case 'd':
+        PlayerY++;
+        break;
+
+    case 'q':
+        bIsRunning = false;
+        break;
+
+    default:
+        break;
+    }
 }
+
+
+void rander() {
+    system("cls");
+
+    for (int y = 0; y < 10; y++) {
+        for (int i = 0; i < 10; i++) {
+            int ivalue = World[y][i];
+
+            if (PlayerX == y && PlayerY == i) {
+                cout << "★";
+            }
+            else {
+                cout << Sprites[ivalue];
+            }
+
+        }
+        cout << endl;
+    }
+}
+
+
+int main_test() {
+      
+    rander();
+    //process
+    while (bIsRunning) {
+        Input();
+        rander();
+    }
+
+    return 0;
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
